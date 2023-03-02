b1626a68c647cce0c24727fc52472c1fa102cdcf
Kimosop-Wakanda <ryderkimosop11@gmail.com>
2023-03-02 13:32:13 +0300

    subnav

---
+++ subnav/subnav.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <link rel="stylesheet" type="text/css" href="css/subnav.css">
+
+</head>
+<body>
+
+    <div>
+        <nav>
+            <a href="">Home</a>
+            <a href="">About</a>
+            <a href="">Blog</a>
+            <a href="">Portfolio</a>
+            <a href="">Contact</a>
+            <div class="animation start-home"></div>
+        </nav>
+    </div>
+
+
+</body>
+</html>
